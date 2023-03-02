3849ccf3ffd1d2a9262ad96a5156b41bdfed603b
ksktharsana@gmail.com <ksktharsana@gmail.com>
2023-03-03 00:49:09 +0530

    changes in blood testing results sql

--- MLT/blood_testing_result.sql
+++ MLT/blood_testing_result.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 09, 2023 at 06:21 PM
+-- Generation Time: Mar 02, 2023 at 08:17 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -29,7 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `blood_testing_result` (
   `test_result_id` int(100) NOT NULL,
-  `donation_id` int(100) NOT NULL,
+  `Hospital_ID` int(50) NOT NULL,
+  `MLT_ID` int(50) NOT NULL,
+  `BloodBankDoctor_ID` int(50) NOT NULL,
+  `donar_id` int(100) NOT NULL,
   `blood_group` varchar(100) NOT NULL,
   `malaria_result` text NOT NULL,
   `hiv_result` text NOT NULL,
@@ -45,22 +48,22 @@ CREATE TABLE `blood_testing_result` (
 -- Dumping data for table `blood_testing_result`
 --
 
-INSERT INTO `blood_testing_result` (`test_result_id`, `donation_id`, `blood_group`, `malaria_result`, `hiv_result`, `hbv_result`, `hcv_result`, `vdrl_result`, `process_date`, `batch_number`, `status`) VALUES
-(1, 1, 'O+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
-(2, 2, 'A-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
-(3, 3, 'B+', 'Positive', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
-(4, 4, 'AB+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
-(5, 5, 'O-', 'Negative', 'Negative', 'Negative', 'Negative', 'Positive', '2023-02-08', 1, 'Pending'),
-(6, 6, 'O-', 'Positive', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
-(7, 7, 'O-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 2, 'Pending'),
-(8, 8, 'O+', 'Positive', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 2, 'Pending'),
-(9, 9, 'A-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 2, 'Pending'),
-(10, 10, 'B-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 2, 'Pending'),
-(11, 11, 'B+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 3, 'Pending'),
-(12, 12, 'O+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 3, 'Pending'),
-(13, 13, 'A+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 3, 'Pending'),
-(14, 14, 'A+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 3, 'Pending'),
-(15, 15, 'O+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 3, 'Pending');
+INSERT INTO `blood_testing_result` (`test_result_id`, `Hospital_ID`, `MLT_ID`, `BloodBankDoctor_ID`, `donar_id`, `blood_group`, `malaria_result`, `hiv_result`, `hbv_result`, `hcv_result`, `vdrl_result`, `process_date`, `batch_number`, `status`) VALUES
+(1, 1, 1, 0, 1, 'O+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
+(2, 1, 1, 0, 2, 'A-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
+(3, 1, 1, 0, 3, 'B+', 'Positive', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
+(4, 1, 1, 0, 4, 'AB+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
+(5, 2, 3, 0, 5, 'O-', 'Negative', 'Negative', 'Negative', 'Negative', 'Positive', '2023-02-08', 1, 'Pending'),
+(6, 2, 3, 0, 6, 'O-', 'Positive', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 1, 'Pending'),
+(7, 1, 1, 0, 7, 'O-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 2, 'Pending'),
+(8, 1, 1, 0, 8, 'O+', 'Positive', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 2, 'Pending'),
+(9, 1, 1, 0, 9, 'A-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 2, 'Pending'),
+(10, 1, 1, 0, 10, 'B-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-08', 2, 'Pending'),
+(16, 2, 3, 0, 16, 'O+', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-02-09', 1, 'Pending'),
+(17, 2, 3, 0, 0, 'A-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-03-02', 1, 'Pending'),
+(18, 2, 3, 0, 0, 'O-', 'Negative', 'Negative', 'Negative', 'Negative', 'Negative', '2023-03-02', 1, 'Pending'),
+(19, 1, 1, 0, 0, 'O-', 'Positive', 'Negative', 'Negative', 'Negative', 'Negative', '2023-03-02', 1, 'Pending'),
+(20, 2, 3, 0, 0, 'O+', 'Negative', 'Positive', 'Negative', 'Negative', 'Negative', '2023-03-02', 1, 'Pending');
 
 --
 -- Indexes for dumped tables
@@ -80,7 +83,7 @@ ALTER TABLE `blood_testing_result`
 -- AUTO_INCREMENT for table `blood_testing_result`
 --
 ALTER TABLE `blood_testing_result`
-  MODIFY `test_result_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `test_result_id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
