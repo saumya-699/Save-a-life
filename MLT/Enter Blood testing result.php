17c10f81e35e7cc91b19e7210525ddf4e3d15dff
ksktharsana@gmail.com <ksktharsana@gmail.com>
2023-03-02 23:35:43 +0530

    modified enter blood teat result php

--- MLT/Enter Blood testing result.php
+++ MLT/Enter Blood testing result.php
@@ -202,7 +202,7 @@
 
                     
                             <label for="exampleFormControlInput1 " class="form-label lbl star ">Donar ID</label>
-                            <input type="text" class="form-control txt-input " name="donation_id"placeholder="Type donation id" required="" >
+                            <input type="text" class="form-control txt-input " name="donar_id"placeholder="Type donar id" required="" >
 
                             
                         <label for="exampleFormControlInput1 " class="form-label lbl star ">Blood Group</label>
