26bf6f5cf0ead1752486f4875ac94b30979416b4
ksktharsana@gmail.com <ksktharsana@gmail.com>
2023-03-03 01:29:09 +0530

    changing

--- MLT/Enter Blood testing result.php
+++ MLT/Enter Blood testing result.php
@@ -263,9 +263,6 @@
                <option value=" " diabled>Select batch_number </option>
                <option value="1">1</option>
                 <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
             
             </select>
 
