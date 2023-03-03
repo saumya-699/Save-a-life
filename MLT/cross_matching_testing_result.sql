d4495612d79195d6dba540049569558851f33d1f
ksktharsana@gmail.com <ksktharsana@gmail.com>
2023-03-03 19:36:03 +0530

    Update cross_matching_testing_result.sql

--- MLT/cross_matching_testing_result.sql
+++ MLT/cross_matching_testing_result.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 02, 2023 at 10:16 PM
+-- Generation Time: Mar 03, 2023 at 03:04 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -32,6 +32,7 @@ CREATE TABLE `cross_matching_testing_result` (
   `request_id` int(50) NOT NULL,
   `MLT_ID` int(50) NOT NULL,
   `Hospital_ID` int(50) NOT NULL,
+  `BloodBankDoctor_ID` int(50) NOT NULL,
   `blood_group` varchar(100) NOT NULL,
   `test_result` text NOT NULL,
   `process_date` date NOT NULL
@@ -41,17 +42,17 @@ CREATE TABLE `cross_matching_testing_result` (
 -- Dumping data for table `cross_matching_testing_result`
 --
 
-INSERT INTO `cross_matching_testing_result` (`test_result_id`, `request_id`, `MLT_ID`, `Hospital_ID`, `blood_group`, `test_result`, `process_date`) VALUES
-(1, 7, 3, 2, 'O-', 'Matched', '2022-11-20'),
-(2, 8, 3, 2, 'A+', 'Matched', '2022-11-20'),
-(3, 9, 4, 5, 'B+', 'Matched', '2022-11-20'),
-(4, 10, 4, 5, 'B+', 'Matched', '2022-11-23'),
-(5, 11, 4, 5, 'B-', 'Matched', '2022-12-05'),
-(6, 12, 4, 5, 'B+', 'Matched', '2022-12-10'),
-(7, 13, 4, 5, 'O-', 'Matched', '2022-12-10'),
-(8, 14, 4, 5, 'B+', 'Matched', '2022-12-10'),
-(9, 65, 3, 2, 'B-', 'Matched', '2023-03-02'),
-(10, 66, 3, 2, 'AB-', 'Matched', '2023-03-02');
+INSERT INTO `cross_matching_testing_result` (`test_result_id`, `request_id`, `MLT_ID`, `Hospital_ID`, `BloodBankDoctor_ID`, `blood_group`, `test_result`, `process_date`) VALUES
+(1, 7, 3, 2, 0, 'O-', 'Matched', '2022-11-20'),
+(2, 8, 3, 2, 0, 'A+', 'Matched', '2022-11-20'),
+(3, 9, 4, 5, 0, 'B+', 'Matched', '2022-11-20'),
+(4, 10, 4, 5, 0, 'B+', 'Matched', '2022-11-23'),
+(5, 11, 4, 5, 0, 'B-', 'Matched', '2022-12-05'),
+(6, 12, 4, 5, 0, 'B+', 'Matched', '2022-12-10'),
+(7, 13, 4, 5, 0, 'O-', 'Matched', '2022-12-10'),
+(8, 14, 4, 5, 0, 'B+', 'Matched', '2022-12-10'),
+(9, 65, 3, 2, 0, 'B-', 'Matched', '2023-03-02'),
+(10, 66, 3, 2, 0, 'AB-', 'Matched', '2023-03-02');
 
 --
 -- Indexes for dumped tables
@@ -71,7 +72,7 @@ ALTER TABLE `cross_matching_testing_result`
 -- AUTO_INCREMENT for table `cross_matching_testing_result`
 --
 ALTER TABLE `cross_matching_testing_result`
-  MODIFY `test_result_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `test_result_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
