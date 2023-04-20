02b0ed961d2064b2d50022db7196370d468efac1
Hiru99 <74122201+hiru-99@users.noreply.github.com>
2023-04-20 20:40:16 +0530

    add medical detail

--- Nurse/add_medical.php
+++ Nurse/add_medical.php
@@ -1,37 +1,164 @@
-
-<?php
-
-require 'see.php'
-
-
-?>
-
-<html>
-    <head>
-        
-        <title>Add medical details</title>
-
-        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/vanillajs-datepicker@1.1.4/dist/css/datepicker.min.css'>
-
-<link rel="stylesheet" href="style_d.css">  
-<link rel="stylesheet" href="css/nbtssl/nbtssl.min.css">
-<link rel="stylesheet" href="css/fontawesome-free-5.15.4/css/all.css">
-<link rel="stylesheet" href="css/mediaquery.css">
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Side bar</title>
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css'>
+<link rel='stylesheet' href='https://unpkg.com/css-pro-layout@1.1.0/dist/css/css-pro-layout.css'>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&amp;display=swap'><link rel="stylesheet" href="./style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script><link rel="stylesheet" href="./add_medical.css">
 
 
-    
 </head>
-    <body>
+<body>
+<!-- partial:index.partial.html -->
+<div class="layout has-sidebar fixed-sidebar fixed-header">
+      <aside id="sidebar" class="sidebar break-point-sm has-bg-image">
+        <a id="btn-collapse" class="sidebar-collapser"><i class="ri-arrow-left-s-line"></i></a>
+        <div class="image-wrapper">
+          <img src="assets/images/sidebar-bg.jpg" alt="sidebar background" />
+        </div>
+        <div class="sidebar-layout">
+          <div class="sidebar-header">
+            <div class="pro-sidebar-logo">
+              <div><img src="logo.png" alt="logo"></div>
+              <h5>Save A Life</h5>
+            </div>
+          </div>
+          <div class="sidebar-content">
+            <nav class="menu open-current-submenu">
+              <ul>
+                <li class="menu-header"></li>
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-home-fill"></i>
+                    </span>
+                    <span class="menu-title">Home</span>
+                    
+                  </a>
+                 </li>
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-user-add-fill"></i>
+                    </span>
+                    <span class="menu-title">Register the Donors</span>
+                  </a>
+                  </li>
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-heart-add-fill"></i>
+                    </span>
+                    <span class="menu-title">Add medical Details</span>
+                  </a>
+                  </li>
+                <li class="menu-item sub-menu">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-folder-user-fill"></i>
+                    </span>
+                    <span class="menu-title">Donor Information</span>
+                  </a>
+                  <div class="sub-menu-list">
+                    <ul>
+                      <li class="menu-item">
+                        <a href="#">
+                          <span class="menu-title">Personal</span>
+                        </a>
+                      </li>
+                      <li class="menu-item">
+                        <a href="#">
+                          <span class="menu-title">Medical</span>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-file-text-fill"></i>
+                    </span>
+                    <span class="menu-title">Declaration Form</span>
+                  </a>
+                </li>
 
-    <div class="container frm-login">
-        <div class="card frm-form">
-            <div class="card-body frm-body">
-                <h1 class="txt-l ">Medical Details</h1>
-                <form method="post" action="add_medical_backend.php">
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-drop-fill"></i>
+                    </span>
+                    <span class="menu-title">Daily Blood Collection</span>
+                  </a>
+                  </li>
+
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-history-line"></i>
+                    </span>
+                    <span class="menu-title">Donation History</span>
+                  </a>
+                  </li>
+                
+
+                <li class="menu-header" style="padding-top: 40px"><span>| </span></li>
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-user-line"></i>
+                    </span>
+                    <span class="menu-title">Profile</span> 
+                  </a>
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-notification-line"></i>
+                    </span>
+                    <span class="menu-title">Notifications</span>
+                  </a>
+                </li>
+                </li>
+                <li class="menu-item">
+                  <a href="#">
+                    <span class="menu-icon">
+                      <i class="ri-logout-box-r-line"></i>
+                    </span>
+                    <span class="menu-title">Log out</span>
+                  </a>
+                </li>
+                
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </aside>
+      <div id="overlay" class="overlay"></div>
+      <div class="layout">
+        <main class="content">
+
+            <a id="btn-toggle" href="#" class="sidebar-toggler break-point-sm"></a>
+            <!-- add your content from here -->
+           
+
+            <div class="container-shadow">
+            </div>
+            <div class="container">
+              <div class="wrap">
+                <div class="headings">
+                  <center><span><h1>Add medical details</h1></span><center>
+                
+                </div>
+               
                     
-                    
-                    <label for="exampleFormControlInput1" class="form-label lbl star">Donor ID</label>
+               <form method="post" action="add_medical_backend.php">
+                  
+                           
+               <label for="exampleFormControlInput1" class="form-label lbl star">Donor ID</label>
                     <input type="text" name="donorid" class="form-control txt-input" placeholder="Type Donor ID" required>
 
                     <label for="exampleFormControlInput1" class="form-label lbl star">Donor Name</label>
@@ -53,22 +180,30 @@ require 'see.php'
 
                     <label for="exampleFormControlInput1" class="form-label lbl star">Hemoglobine Level</label>
                     <input type="text" class="form-control txt-input" placeholder="Hemoglobine Level" name="hemoglobine" required>
-
-                
-                    
-
-                    <div class="row btn-buttons">
-                        <input type="hidden" name="registration" value="success"/>
-                        <div class="col btn-but"> <input type="submit" value="Add" class="btn btn-danger btn-reg"  name="BtnSubmit"></div>
-                        <div class="col btn-but"> <input type="submit" value="Cancel" class="btn btn-secondary btn-can"></div>
+                                        
+                                      <br><br><br><br>
+                         
+                            <div class='row btn-buttons'>
+                                  
+                                   <input type='submit' name='BtnSubmit' value='Add' class='b1'>
+                                   <input type='submit' name='btnCancel' value='Cancel' class='b2'>
+                              </div>							  
+          
+               
                     </div>
-
-                </form>
-
+              
+          
+             
             </div>
-        </div>
+           
+              </div>
+                </form> 
+          
+        </main>
+      </div>
     </div>
-
+<!-- partial -->
+<script src='https://unpkg.com/@popperjs/core@2'></script><script  src="./script.js"></script>
 
 </body>
 </html>
