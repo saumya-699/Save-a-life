6e206858d17c717df42af1230d253bd1b95e1aab
Hiru99 <74122201+hiru-99@users.noreply.github.com>
2023-05-15 05:36:23 +0530

    Update change_password_backend.php

--- Nurse/change_password_backend.php
+++ Nurse/change_password_backend.php
@@ -64,7 +64,7 @@ if (isset($_POST['submit'])) {
         } else {
             // Old password does not match
             $_SESSION['old-pwd-not-match'] = true;
-            echo "<script>alert('Old password does not match.'); window.location.href = 'password_change.php';</script>";
+            echo "<script>alert('Current password does not match.'); window.location.href = 'password_change.php';</script>";
             exit();
         }
     } else {
